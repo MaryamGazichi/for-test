3ae4444ff2586bf419a766daa7f031e085d88337
Maryamgazichi <maryamgazichi@gmail.com>
2024-07-18 19:25:41 +0330

    add4

---
+++ a.sh
@@ -0,0 +1,44 @@
+# 1
+cd /home/maryamdesk/exe6
+git init
+git config --global user.name "Maryamgazichi"
+git config --global user.email "maryamgazichi@gmail.com"
+echo "# It is for my MLOPs class." > README.md
+git add README.md
+git status
+git commit -m "Initial commit"
+git log
+
+
+# 2
+
+git branch feature-branch
+git checkout feature-branch
+echo "This is maryam" > feature.txt
+git add feature.txt
+git commit -m "Add feature.txt with initial content"
+
+# 3
+
+git blame -L '/def process_data/',+1 -- app.py
+git show abcdef123456
+
+# 4
+git clean -n
+git clean -f
+
+# 5
+ssh-keygen -t rsa -b 4096 -C "maryamgazichi@gmail.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/rsa
+pbcopy < ~/.ssh/rsa.pub
+ssh -T git@github.com
+
+# 6
+git checkout feature-branch
+git push origin feature-branch
+
+# 7
+git checkout main
+git merge feature-branch
+git push origin main 
